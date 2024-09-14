6a6ff48a71e0e6f7f853fdcf19cb93de84b0a5b2
bigybags <bigybags@gmail.com>
2024-09-14 10:29:33 +0500

    URL set

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
-// export const API_URL = 'https://mustafahasnain19.pythonanywhere.com/api';
+// export const API_URL = 'http://localhost:8000/api';
+export const API_URL = 'https://mustafahasnain19.pythonanywhere.com/api';
